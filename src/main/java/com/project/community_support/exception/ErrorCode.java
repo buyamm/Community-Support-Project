befd1f165e056458523b4586f71f6844542594ec
Truong Cong Ly <truongcongly139@gmail.com>
2024-11-20 09:16:46 +0700

    add dockerfile

--- src/main/java/com/project/community_support/exception/ErrorCode.java
+++ src/main/java/com/project/community_support/exception/ErrorCode.java
@@ -7,21 +7,18 @@ import org.springframework.http.HttpStatusCode;
 
 @Getter
 public enum ErrorCode {
-    USER_EXISTED(1001, "User existed with this phone number or this identifier number", HttpStatus.BAD_REQUEST),
-    USER_NOT_FOUND(1002, "User not found!", HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1001, "User existed with this phone number or this identifier number", HttpStatus.CONFLICT),
 
-    ORGANIZATION_NOT_FOUND(1002, "Organization not found!", HttpStatus.BAD_REQUEST),
+    USER_NOT_FOUND(1002, "User not found!", HttpStatus.NOT_FOUND),
+    ORGANIZATION_NOT_FOUND(1002, "Organization not found!", HttpStatus.NOT_FOUND),
+    FORM_NOT_FOUND(1002, "Form not found!", HttpStatus.NOT_FOUND),
 
-    FORM_NOT_FOUND(1002, "Form not found!", HttpStatus.BAD_REQUEST),
-
-    WRONG_PHONE_NUMBER(1003, "Wrong phone number",HttpStatus.BAD_REQUEST),
-    WRONG_PASSWORD(1004, "Wrong password",HttpStatus.BAD_REQUEST),
+    WRONG_PHONE_NUMBER(1003, "Wrong phone number",HttpStatus.UNAUTHORIZED),
+    WRONG_PASSWORD(1004, "Wrong password",HttpStatus.UNAUTHORIZED),
 
     PHONE_NUMBER_INVALID(1005, "Phone number must be 10 character", HttpStatus.BAD_REQUEST),
     PASSWORD_INVALID(1006, "Password must be at least 8 character", HttpStatus.BAD_REQUEST),
     CCCD_INVALID(1007, "CCCD must be at least 12 character", HttpStatus.BAD_REQUEST),
-
-
     INVALID_KEY(8888, "Invalid message key", HttpStatus.BAD_REQUEST),
 //    UNAUTHENTICATED(9999, "", HttpStatus.BAD_REQUEST);
     ;
